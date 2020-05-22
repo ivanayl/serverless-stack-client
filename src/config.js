165dc70758c4bb0425b0ba21ec2ac5085503b124
Ivana Lin <ivana.yl28@gmail.com>
2020-05-21 22:43:45 -0400

    Building our React app

--- src/config.js
+++ src/config.js
@@ -1,16 +1,17 @@
 export default {
   s3: {
-    REGION: "YOUR_S3_UPLOADS_BUCKET_REGION",
-    BUCKET: "YOUR_S3_UPLOADS_BUCKET_NAME"
+    REGION: "us-east-2",
+    BUCKET: "ivana-notes-uploads"
   },
   apiGateway: {
-    REGION: "YOUR_API_GATEWAY_REGION",
-    URL: "YOUR_API_GATEWAY_URL"
+    REGION: "us-east-2",
+    URL: "https://ubwyqkbykc.execute-api.us-east-2.amazonaws.com/prod"
   },
   cognito: {
-    REGION: "YOUR_COGNITO_REGION",
-    USER_POOL_ID: "YOUR_COGNITO_USER_POOL_ID",
-    APP_CLIENT_ID: "YOUR_COGNITO_APP_CLIENT_ID",
-    IDENTITY_POOL_ID: "YOUR_IDENTITY_POOL_ID"
-  }
+    REGION: "us-east-2",
+    USER_POOL_ID: "us-east-2_oSxo32zzp",
+    APP_CLIENT_ID: "1d9l159erm9et3av9bno1vlp9g",
+    IDENTITY_POOL_ID: "us-east-2:39596522-2b3c-4e84-a674-3656388a2417"
+  },
+  STRIPE_KEY: "pk_test_fxs6C8BycpN4o0t9gtW6CTzI00dwh3yw14",
 };
